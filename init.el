f918673af55284e8d4cc90b0d0454d552a0f35b6
joanimato <joani.mato@ucdenver.edu>
2020-05-15 14:13:08 -0600

    error fix

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line-inactive ((t (:background "#171717" :foreground "#6a6d62" :box (:line-width 2 :color "grey75" :style released-button)))))
+ '(mode-line-inactive ((t (:background "#171717" :foreground "#6a6d62" :box (:line-width 2 :color "grey75" :style released-button))))))
 
 
 (require 'package)
