bb0ff771e4a5013ca3d94bf7fdc104cd40625165
joanimato <joani.mato@ucdenver.edu>
2020-05-16 23:38:47 -0600

    treemacs and dictionary

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
     ("76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "1068ae7acf99967cc322831589497fee6fb430490147ca12ca7dd3e38d9b552a" "f9cae16fd084c64bf0a9de797ef9caedc9ff4d463dd0288c30a3f89ecf36ca7e" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" default)))
  '(package-selected-packages
    (quote
-    (general org-bullets flycheck wheatgrass zerodark-theme ivy swiper evil rainbow-delimiters spaceline smex ido-vertical-mode spacemacs-theme doom-modeline all-the-icons doom-themes which-key use-package))))
+    (treemacs-icons-dired treemacs-evil treemacs dired-subtree dired-narrow general org-bullets flycheck wheatgrass zerodark-theme ivy swiper evil rainbow-delimiters spaceline smex ido-vertical-mode spacemacs-theme doom-modeline all-the-icons doom-themes which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
